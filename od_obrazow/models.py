57afee072a255b0c1210345228680589c0c795d1
Arjissuan <nswa87@gmail.com>
2022-04-05 17:09:41 +0200

    Usprawnione multimedia

--- od_obrazow/models.py
+++ od_obrazow/models.py
@@ -1,6 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 class MultiMedia(models.Model):
     title = models.CharField(max_length=250)
-    image = models.ImageField(upload_to='media/')
+    when = models.DateTimeField('added', default=timezone.now())
+    image = models.ImageField(upload_to='image/')
+
+    def __str__(self):
+        return self.title
+
 # Create your models here.
